ecf33cc0cbcef6f5ab087fc59657b03f84848ced
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-21 23:13:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    html = open('./fixtures/student-site/index.html')
+    html = open('')
     doc = Nokogiri::HTML(html)
     
     scraped_students = []
